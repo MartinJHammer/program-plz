7e3c70751d0bbcffed67ee163136c2936db91665
MartinJHammer <martin_hammer@live.dk>
2019-03-23 22:46:06 +0100

    - Removed navbar - Added exercise detail modal - Put exercises in bootstrap cards - Added ionicons - Removed popper.js

--- src/app/components/program/program.component.ts
+++ src/app/components/program/program.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Program } from 'src/app/models/program';
 import { Exercise } from 'src/app/models/exercise';
 
+declare var $: any;
+
 @Component({
   selector: 'program-plz-program',
   templateUrl: './program.component.html',
   styleUrls: ['./program.component.scss']
 })
 export class ProgramComponent implements OnInit {
+
+  public currentExercise: Exercise;
+
   constructor(
     public program: Program
   ) { }
@@ -19,8 +24,16 @@ export class ProgramComponent implements OnInit {
     this.program.createProgram();
   }
 
-  public differentVersion(exercise: Exercise) {
-    this.program.differentVersion(exercise);
+  public differentVersion(exercise?: Exercise) {
+    exercise ? this.program.differentVersion(exercise) : this.program.differentVersion(this.currentExercise);
+  }
+
+  public setCurrentExercise(exercise: Exercise) {
+    this.currentExercise = exercise;
+  }
+
+  public closeExerciseModal() {
+    $('#exerciseModal').modal('hide');
   }
 
   public trackById(index, item) {
