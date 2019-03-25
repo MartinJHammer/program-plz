c5f088240ef98c9946584732b3561f8dd5161322
MartinJHammer <martin_hammer@live.dk>
2019-03-25 23:35:57 +0100

    Edit now works.

--- src/app/components/exercises-edit/exercises-edit.component.ts
+++ src/app/components/exercises-edit/exercises-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Exercise } from 'src/app/models/exercise';
+import { map, switchMap, mergeAll, take } from 'rxjs/operators';
 
 @Component({
   selector: 'pp-exercises-edit',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExercisesEditComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+
+  constructor(
+    public db: AngularFirestore,
+    public fb: FormBuilder,
+    public router: Router,
+    public activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.pipe(
+      map(params => params['id']),
+      switchMap(id => this.db.doc<Exercise>(`exercises/${id}`).get()),
+      map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      } as Exercise)),
+      map(exercise => this.form = this.fb.group({
+        id: exercise.id,
+        name: exercise.name
+      })),
+      take(1)
+    ).subscribe();
   }
 
+  onSubmit() {
+    const exercise: Exercise = this.form.value;
+    this.db.doc(`exercises/${exercise.id}`).update(exercise);
+    this.router.navigate(['exercises']);
+  }
 }
