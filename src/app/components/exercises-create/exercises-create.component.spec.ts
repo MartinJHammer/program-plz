68736c1690b0685aff45e96f469f3c71481a8726
MartinJHammer <martin_hammer@live.dk>
2019-03-25 23:06:18 +0100

    Added create/edit components for exercises.

---
+++ src/app/components/exercises-create/exercises-create.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExercisesCreateComponent } from './exercises-create.component';
+
+describe('ExercisesCreateComponent', () => {
+  let component: ExercisesCreateComponent;
+  let fixture: ComponentFixture<ExercisesCreateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExercisesCreateComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExercisesCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/exercises-edit/exercises-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pp-exercises-edit',
+  templateUrl: './exercises-edit.component.html',
+  styleUrls: ['./exercises-edit.component.scss']
+})
+export class ExercisesEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
