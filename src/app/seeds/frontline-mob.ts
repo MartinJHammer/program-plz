1f7d1ae4b5c9d5778104689ce9c7c88dba56f5e4
martin_hammer <martin_hammer@live.dk>
2019-03-30 22:27:20 +0100

    - Styled delete links. - Removed seeds.

--- src/app/seeds/frontline-mob.ts
+++
@@ -1,8 +0,0 @@
-import { Exercise } from 'src/app/models/exercise';
-import { ExerciseType } from 'src/app/models/exercise-type';
-
-export const frontlineMobExercises: Exercise[] = [
-    new Exercise({
-        name: 'Standing quad stretch'
-    })
-];
