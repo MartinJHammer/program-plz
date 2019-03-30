2571ffb38458afb5bb221c67ff774d2c01d606b8
martin_hammer <martin_hammer@live.dk>
2019-03-30 21:42:37 +0100

    - Added form component - Fixed wrong prefix rule

---
+++ src/app/components/components.ts
@@ -0,0 +1,25 @@
+import { ExercisesIndexComponent } from './exercises/exercises-index/exercises-index.component';
+import { ProgramComponent } from './program/program.component';
+import { ExercisesCreateComponent } from './exercises/exercises-create/exercises-create.component';
+import { ExercisesEditComponent } from './exercises/exercises-edit/exercises-edit.component';
+import { ManageComponent } from './manage/manage.component';
+import { ExerciseTypesIndexComponent } from './exercise-types/exercise-types-index/exercise-types-index.component';
+import { ExerciseTypesCreateComponent } from './exercise-types/exercise-types-create/exercise-types-create.component';
+import { ExerciseTypesEditComponent } from './exercise-types/exercise-types-edit/exercise-types-edit.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { StartComponent } from './start/start.component';
+import { FormComponent } from './form/form/form.component';
+
+export const components = [
+    StartComponent,
+    ProgramComponent,
+    ManageComponent,
+    NotFoundComponent,
+    ExercisesIndexComponent,
+    ExercisesCreateComponent,
+    ExercisesEditComponent,
+    ExerciseTypesIndexComponent,
+    ExerciseTypesCreateComponent,
+    ExerciseTypesEditComponent,
+    FormComponent
+];
--- src/app/start/app.module.ts
+++ src/app/start/app.module.ts
@@ -3,20 +3,12 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-
 import { AppRoutingModule } from './app-routing.module';
-import { ExercisesIndexComponent } from './../components/exercises/exercises-index/exercises-index.component';
-import { ProgramComponent } from './../components/program/program.component';
 import { StartComponent } from './../components/start/start.component';
 import { environment } from 'src/environments/environment';
-import { ExercisesCreateComponent } from './../components/exercises/exercises-create/exercises-create.component';
-import { ExercisesEditComponent } from './../components/exercises/exercises-edit/exercises-edit.component';
-import { ManageComponent } from '../components/manage/manage.component';
-import { ExerciseTypesIndexComponent } from '../components/exercise-types/exercise-types-index/exercise-types-index.component';
-import { ExerciseTypesCreateComponent } from '../components/exercise-types/exercise-types-create/exercise-types-create.component';
-import { ExerciseTypesEditComponent } from '../components/exercise-types/exercise-types-edit/exercise-types-edit.component';
-import { NotFoundComponent } from '../components/not-found/not-found.component';
+
 import { pipes } from '../pipes/pipes';
+import { components } from '../components/components';
 
 @NgModule({
   imports: [
@@ -27,16 +19,7 @@ import { pipes } from '../pipes/pipes';
     AngularFirestoreModule
   ],
   declarations: [
-    StartComponent,
-    ProgramComponent,
-    ManageComponent,
-    NotFoundComponent,
-    ExercisesIndexComponent,
-    ExercisesCreateComponent,
-    ExercisesEditComponent,
-    ExerciseTypesIndexComponent,
-    ExerciseTypesCreateComponent,
-    ExerciseTypesEditComponent,
+    ...components,
     ...pipes
   ],
   providers: [],
