acd62957d1ad7faef2894996e1ce18593e26b970
MartinJHammer <martin_hammer@live.dk>
2019-04-19 22:14:28 +0200

    Made exercise types index use infinite list.

--- src/app/components/exercise-types/exercise-types-index/exercise-types-index.component.ts
+++ src/app/components/exercise-types/exercise-types-index/exercise-types-index.component.ts
@@ -1,26 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Exercise } from 'src/app/models/exercise';
-import { Observable } from 'rxjs';
-import { DatabaseService } from 'src/app/services/database.service';
-import { ExerciseType } from 'src/app/models/exercise-type';
-import { ExerciseTypeService } from 'src/app/services/exercise-type.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pp-exercise-types',
   templateUrl: './exercise-types-index.component.html',
   styleUrls: ['./exercise-types-index.component.scss']
 })
-export class ExerciseTypesIndexComponent implements OnInit {
-
-  public exerciseTypes: Observable<ExerciseType[]>;
-
-  constructor(public db: DatabaseService<ExerciseType>, public service: ExerciseTypeService) { }
-
-  ngOnInit() {
-    this.exerciseTypes = this.service.getAll();
-  }
-
-  public delete(exercise: Exercise) {
-    this.db.delete(`exercise-types/${exercise.id}`);
-  }
-}
+export class ExerciseTypesIndexComponent { }
--- src/app/components/exercises/exercises-index/exercises-index.component.ts
+++ src/app/components/exercises/exercises-index/exercises-index.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { CrudService } from 'src/app/crud/crud.service';
 
 @Component({
   selector: 'pp-exercises',
   templateUrl: './exercises-index.component.html',
   styleUrls: ['./exercises-index.component.scss']
 })
-export class ExercisesIndexComponent {
-  constructor(
-    public afs: AngularFirestore,
-    public crudService: CrudService
-  ) {
-  }
-}
+export class ExercisesIndexComponent { }
