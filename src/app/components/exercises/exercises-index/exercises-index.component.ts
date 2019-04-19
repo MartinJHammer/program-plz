de678c6986614d85bf21c6b720691d74c916d227
MartinJHammer <martin_hammer@live.dk>
2019-04-19 21:23:31 +0200

    Infinite scroll now works (and delete and edit doesn't...)

--- src/app/components/exercises/exercises-index/exercises-index.component.ts
+++ src/app/components/exercises/exercises-index/exercises-index.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { Exercise } from 'src/app/models/exercise';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { DatabaseService } from 'src/app/services/database.service';
 import { ExerciseService } from 'src/app/services/exercise.service';
-import { map, switchMap } from 'rxjs/operators';
+import { map, tap, throttleTime, mergeMap, scan } from 'rxjs/operators';
 import { CrudService } from 'src/app/services/crud.service';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'program-plz-exercises',
@@ -13,30 +15,78 @@ import { CrudService } from 'src/app/services/crud.service';
 })
 export class ExercisesIndexComponent implements OnInit {
 
-  public exercises: Observable<Exercise[]>;
+  // Infinite
+  @ViewChild(CdkVirtualScrollViewport) public viewport: CdkVirtualScrollViewport;
+  public theEnd = false;
+  public offset = new BehaviorSubject(null);
+  public infinite$: Observable<any>;
+  public batchSize = 10;
+
+
+
+  // public exercises: Observable<Exercise[]>;
   public currentExercise: Exercise;
 
   constructor(
     public db: DatabaseService<Exercise>,
+    public afs: AngularFirestore,
     public service: ExerciseService,
     public crudService: CrudService
   ) { }
 
   ngOnInit() {
-    this.exercises = this.service.get().pipe(map(ex => {
-      return ex.sort((a, b) => (a.name > b.name) ? 1 : -1);
-    }));
+    // this.exercises = this.service.get().pipe(map(ex => {
+    //   return ex.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    // }));
+
+    this.initInfinite();
   }
 
-  public nextPage(index: number): void {
-    // TODO: THIS IS BAD - TOO MANY REQUESTS ETC.
-    // SEE https://angularfirebase.com/lessons/infinite-scroll-firestore-angular/ INSTEAD
-    this.exercises = this.exercises.pipe(
-      switchMap(values => {
-        return this.service.get().pipe(map(newValues => values.concat(newValues)));
+  public initInfinite(): void {
+    const batchMap = this.offset.pipe(
+      throttleTime(500),
+      mergeMap(x => this.getBatch(x)),
+      scan((acc, batch) => {
+        return { ...acc, ...batch };
+      })
+    );
+
+    this.infinite$ = batchMap.pipe(
+      map(x => Object.values(x))
+    );
+  }
+
+  public getBatch(lastSeen: string) {
+    return this.afs.collection('exercises', ref => ref.orderBy('name').startAfter(lastSeen).limit(this.batchSize)).snapshotChanges().pipe(
+      tap(arr => (arr.length ? null : (this.theEnd = true))),
+      map(arr => {
+        return arr.reduce((acc, cur) => {
+          const id = cur.payload.doc.id;
+          const data = cur.payload.doc.data();
+          return { ...acc, [id]: data };
+        }, {});
       })
     );
   }
+
+  public nextBatch(event: any, offset: number): void {
+    if (this.theEnd) {
+      return;
+    }
+
+    const end = this.viewport.getRenderedRange().end;
+    const total = this.viewport.getDataLength();
+
+    if (end === total) {
+      this.offset.next(offset);
+    }
+  }
+
+  public trackByIndex(index: number) {
+    return index;
+  }
+
+
 
   public setCurrentExercise(exercise: Exercise): void {
     this.currentExercise = exercise;
