2eba1b493ed71204a1eee538b4845b1935818aad
MartinJHammer <martin_hammer@live.dk>
2019-05-05 12:35:02 +0200

    Random exercise via firestore almost works.

--- src/app/components/program/program.component.ts
+++ src/app/components/program/program.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Exercise } from 'src/app/models/exercise';
-import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
+import { Observable, BehaviorSubject, combineLatest, merge, empty, of } from 'rxjs';
 import { ExerciseType } from 'src/app/models/exercise-type';
-import { map, shareReplay, take } from 'rxjs/operators';
+import { map, shareReplay, take, tap, switchMap, scan, filter, expand } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { DatabaseService } from 'src/app/services/database.service';
 
@@ -13,10 +13,9 @@ import { DatabaseService } from 'src/app/services/database.service';
 })
 export class ProgramComponent implements OnInit {
 
-  public allExercises$: Observable<Exercise[]>;
   public allExerciseTypes$: Observable<ExerciseType[]>;
   public currentExercise: Exercise;
-  public exercises$: BehaviorSubject<Exercise[]>;
+  public exercises$ = new BehaviorSubject<Exercise[]>([]);
   public selectedExerciseTypes$ = new BehaviorSubject<ExerciseType[]>([]);
 
   constructor(
@@ -25,34 +24,48 @@ export class ProgramComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.allExercises$ = this.db.getAll('exercises').pipe(shareReplay(1));
     this.allExerciseTypes$ = this.db.getAll('exercise-types').pipe(shareReplay(1));
     const setInitialExerciseTypes = this.allExerciseTypes$.pipe(map(exerciseTypes => this.selectedExerciseTypes$.next(exerciseTypes)), take(1));
     setInitialExerciseTypes.subscribe();
   }
 
   public createProgram(): void {
-    this.exercises$ = new BehaviorSubject<Exercise[]>([]);
-    combineLatest(
-      this.allExercises$,
-      this.selectedExerciseTypes$
-    ).pipe(
-      map(values => {
-        const exercises: Exercise[] = values[0];
-        const exerciseTypes: ExerciseType[] = values[1];
-
-        let allExercises = [];
-
-        exerciseTypes.forEach(et => {
-          const found = exercises.filter(exercise => exercise.exerciseTypes.includes(et.id));
-          if (found) {
-            allExercises = allExercises.concat(shuffle(found).slice(0, 1));
-          }
-        });
-
-        this.exercises$.next(allExercises);
-      })
+    this.selectedExerciseTypes$.pipe(
+      switchMap(exerciseTypes => {
+        return combineLatest(exerciseTypes.map(exerciseType => this.getRandom(exerciseType.id)));
+      }),
+      map(exercises => this.exercises$.next(exercises.reduce((a, b) => a.concat(b), []))),
+      take(1)
     ).subscribe();
+  }
+
+  public getRandom(id: string): Observable<any[]> {
+    const randomId = this.afs.createId();
+    const random$ = this.afs.collection('exercises', ref => ref.where('id', '>=', randomId).where('exerciseTypes', 'array-contains', id).orderBy('id').limit(1)).get().pipe(
+      map(docs => {
+        return docs.docs.map(e => {
+          return {
+            id: e.id,
+            ...e.data()
+          } as any;
+        });
+      })
+    );
+
+    // const retryRequest$ = random$.pipe(
+    //   expand(values => (values === undefined && values[0] === undefined) ? random$ : empty()),
+    //   tap(x => console.log(x)),
+    //   tap(() => randomId = this.afs.createId())
+    // ).subscribe();
+
+    // return of([]);
+
+    const retry$ = random$.pipe(
+      filter(x => x === undefined || x[0] === undefined),
+    );
+
+    const randomRetrieved$ = random$.pipe(filter(x => x !== undefined && x[0] !== undefined));
+    return merge(randomRetrieved$, retry$).pipe(tap(x => console.log('RESULT', x, x.length)));
   }
 
   public setCurrentExercise(exercise: Exercise) {
@@ -68,21 +81,21 @@ export class ProgramComponent implements OnInit {
    * Exercise is of same difficulty and targets same muscles (roughly)
    */
   public differentVersion(currentExercises: Exercise[], exercise: Exercise) {
-    this.allExercises$.pipe(
-      map(exercises => {
-        exercises
-          .filter(ex => exercise.exerciseTypes.some(condition => ex.exerciseTypes.includes(condition)))
-          .filter(ex => ex.id !== exercise.id);
-        const newExercise = shuffle(exercises).slice(0, 1)[0];
-        currentExercises.forEach((ex, index) => {
-          if (ex.id === exercise.id) {
-            currentExercises[index] = newExercise;
-          }
-        });
-        this.exercises$.next(currentExercises);
-      }),
-      take(1)
-    ).subscribe();
+    // this.allExercises$.pipe(
+    //   map(exercises => {
+    //     exercises
+    //       .filter(ex => exercise.exerciseTypes.some(condition => ex.exerciseTypes.includes(condition)))
+    //       .filter(ex => ex.id !== exercise.id);
+    //     const newExercise = shuffle(exercises).slice(0, 1)[0];
+    //     currentExercises.forEach((ex, index) => {
+    //       if (ex.id === exercise.id) {
+    //         currentExercises[index] = newExercise;
+    //       }
+    //     });
+    //     this.exercises$.next(currentExercises);
+    //   }),
+    //   take(1)
+    // ).subscribe();
   }
 
   /**
@@ -111,3 +124,11 @@ function shuffle(array) {
   }
   return array;
 }
+
+// Kept for reference if I have to do offline shuffles. P.T. not used since I shuffle on db level.
+// exerciseTypes.forEach(et => {
+//   const found = exercises.filter(exercise => exercise.exerciseTypes.includes(et.id));
+//   if (found) {
+//     allExercises = allExercises.concat(shuffle(found).slice(0, 1));
+//   }
+// });
